9088332a7073e02b225c65481bbf5f1258c4a781
Paulo César de Carvalho <paulocesarcarvalho@rocketmail.com>
2026-05-17 21:38:25 -0300

    1
    2

---
+++ JS/regras.js
@@ -0,0 +1,13 @@
+
+let btnCopiar = document.querySelector("#btn-copiar")
+
+
+
+btnCopiar.addEventListener("click", async function copiarTexto() {
+  try {
+    await navigator.clipboard.writeText("00020126330014br.gov.bcb.pix0111005827936965204000053039865406100.005802BR5923PAULO CESAR DE CARVALHO6012POUSO ALEGRE62070503***630484C0");
+    alert("Código Pix Copiado com Sucesso");
+  } catch (err) {
+    console.error("Falha ao copiar: ", err);
+  }
+})
