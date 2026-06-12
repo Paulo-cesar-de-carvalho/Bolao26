05d13ff2e734d2000942ed3caf51e266691a0b2c
Tales Carvalho <tales-h-carvalho@hotmail.com>
2026-06-11 18:32:07 -0600

    barra de navegacao: adicionar arquivos como link externo

--- js/componente_barra_navegacao.js
+++ js/componente_barra_navegacao.js
@@ -1,16 +1,27 @@
-const items = {
-    "Classificação Geral": "index.html",
-    Apostas: "apostas.html",
-    Regras: "regras.html",
-}
+const ICONE_LINK_EXTERNO = '<i class="bi bi-box-arrow-up-right"></i>'
+
+const items = [
+    { nome: "Classificação Geral", url: "index.html", externo: false },
+    { nome: "Apostas", url: "apostas.html", externo: false },
+    { nome: "Regras", url: "regras.html", externo: false },
+    {
+        nome: "Arquivos",
+        url: "https://1drv.ms/f/c/2c440c9e2e64aebf/IgC0-tZhXpvxQ68j9fPfTmxKAbsgjwBVMtT7LLH4MKGi-Uw?e=WDPLHt",
+        externo: true,
+    },
+]
 
 function construirBarraDeNavegacao() {
     const navbarDiv = document.getElementById("navbarMain")
 
     let itemsHTML = ""
-    for (const [nome, pagina] of Object.entries(items)) {
+    for (const { nome, url, externo } of items) {
+        let internoHTML = nome
+        if (externo) {
+            internoHTML += " " + ICONE_LINK_EXTERNO
+        }
         itemsHTML += `<li class="nav-item">
-                <a class="nav-link" href="${pagina}">${nome}</a>
+                <a class="nav-link" href="${url}">${internoHTML}</a>
             </li>`
     }
 
